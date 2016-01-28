eeae81820e13b69ba815270d9eeb85bfe1c6af33
zhengguorong <zhengguorong@bluemoon.com.cn>
2016-01-28 11:52:00 +0800

    add article list page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Suzy' });
+
+router.get('/',function(req,res,next){
+  res.render('articlelist', { title: 'Suzy' });
+})
+router.get('/detail', function(req, res, next) {
+  res.render('detail', { title: 'Suzy' });
 });
 
 module.exports = router;
