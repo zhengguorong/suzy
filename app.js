eb5c02016101f370b02ddb91b5e079f6ed43caca
郑国榕 <zhengguorong@bluemoon.com.cn>
2016-08-01 09:21:58 +0800

    调整百度云

--- app.js
+++ app.js
@@ -10,8 +10,14 @@ var routes = require('./routes/index');
 
 var app = express();
 
+var db_name = 'AaWPLImcIQRcrDyjTfVx'; //数据库名称
+var db_host = 'mongo.duapp.com'; //数据库地址
+var db_port = '8908'; // 数据库端口
+var username = 'c690f487e80941de9c917f59724e6051'; //用户AK
+var password = '7caba0a798da489b93853c597235b20c'; //用户SK
+
 //创建数据库连接
-mongoose.connect('mongodb://localhost/suzy');
+mongoose.connect('mongodb://'+username+':'+password+'@'+db_host+':'+db_port+'/'+db_name);
 var db = mongoose.connection;
 db.on('error',function(){
   console.log('connect error');
@@ -60,6 +66,10 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
+var server = app.listen(18080, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log(' app listening at http://%s:%s', host, port);
+});
 
 module.exports = app;
