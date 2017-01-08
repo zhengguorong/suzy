39ed0ba9d193d3f6bf5c2af861d9aee5551ba886
郑国榕 <zhengguorong@bluemoon.com.cn>
2017-01-08 16:27:22 +0800

    修改返回上传文件信息

--- api/file/file.controller.js
+++ api/file/file.controller.js
@@ -11,7 +11,7 @@ controller.upload = function (req, res) {
     if (err) {
       res.send(500, 'parse error: ' + err);
     } else {
-      res.send(200, {files: files});
+      res.send(200, files);
     }
   })
 }
